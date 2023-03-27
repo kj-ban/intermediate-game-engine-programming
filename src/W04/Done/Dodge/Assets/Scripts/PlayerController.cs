5d3bdbbd9b3fa9139200d33dab29bb83e5d57da5
Jason.Van <kjban@maniamind.com>
2023-03-27 14:12:42 +0900

    modified w04 src

--- src/W04/Done/Dodge/Assets/Scripts/PlayerController.cs
+++ src/W04/Done/Dodge/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour 
+{
     private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; // 이동 속력
 
-    void Start() {
+    void Start() 
+    {
         // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update() {
+    void Update() 
+    {
         // 수평과 수직 축 입력 값을 감지하여 저장
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
@@ -29,5 +31,10 @@ public class PlayerController : MonoBehaviour {
     public void Die() {
         // 자신의 게임 오브젝트를 비활성화
         gameObject.SetActive(false);
+
+        // 씬에 존재하는 GameManager 타입의 오브젝트를 찾아서 가져오기
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        // 가져온 GameManager 오브젝트의 EndGame() 메서드 실행
+        gameManager.EndGame();
     }
 }
